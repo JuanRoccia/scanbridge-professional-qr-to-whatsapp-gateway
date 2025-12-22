fec24a1775acdb9e6e8d25e1cc948b58f1900278
Vibesdk <vibesdk-bot@cloudflare.com>
2025-12-22 14:48:11 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add html5-qrcode@^2.3.8"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
